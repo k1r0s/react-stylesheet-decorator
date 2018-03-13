a59321db351e49a6bc225e205ceb05810578022f
k1r0s <ciroreed@gmail.com>
2018-03-13 14:50:33 +0100

    support functional components

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-import * as React from 'react';
+import React from "react"
 import { afterMethod } from "kaop-ts";
 import decamelize from "decamelize";
 import scope from "scope-css";
 
-export const stylesheet = (styleContent) =>
-afterMethod((meta) => {
+const getTag = target => {
+  if(target.__stylesheetTagName) return target.__stylesheetTagName;
+  const uid = Math.random().toString(32).split(".").pop();
+  return target.__stylesheetTagName = `element-${decamelize(target.name, "-")}-${uid}`;
+}
 
-  // create vnode stylesheet only once
-  if(!meta.scope.__stylesheetVNode){
-    meta.scope.__stylesheetTagName = decamelize(meta.target.constructor.name, "-");
+const getProps = props => Object.assign({}, props, { key: "component" });
 
-    // remove all spaces, eols
-    styleContent = styleContent.replace(/(\r\n\s|\n|\r)/gm, "");
+const getStylesheet = (target, stylesheet) => {
+  if(target.__stylesheetVNode) return target.__stylesheetVNode;
+  stylesheet = stylesheet.replace(/(\r\n|\n|\r)/gm, "");
 
-    // prefix all selectors to make stylesheet 'scoped' using scope-css package
-    styleContent = scope(styleContent, meta.scope.__stylesheetTagName);
+  // prefix all selectors to make stylesheet 'scoped' using scope-css package
+  stylesheet = scope(stylesheet, target.__stylesheetTagName);
 
-    // save a reference of the stylesheet within the class instance
-    meta.scope.__stylesheetVNode = React.createElement("style", { key: "scoped" }, styleContent);
-  }
+  // save a reference of the stylesheet within the class instance
+  return target.__stylesheetVNode = React.createElement("style", { scoped: true, key: "scoped" }, stylesheet);
+}
 
-  const newProps = Object.assign({}, meta.result.props, { key: "component" });
-  const fakeNode = React.createElement(meta.result.type, newProps, meta.result.props.children);
+const renderStylesheet = styleContent => afterMethod((meta) => {
+  const tag = getTag(meta.target.constructor);
+  const stylesheetNode = getStylesheet(meta.target.constructor, styleContent);
 
-  // wrap rendered vnode with a parent vnode
-  meta.result = React.createElement(
-    meta.scope.__stylesheetTagName, null,
-    [ fakeNode, meta.scope.__stylesheetVNode ]
-  );
-
+  const fakeNode = React.createElement(meta.result.type, getProps(meta.result.props), meta.result.props.children);
+  // wrap rendered vnode with a hoc
+  meta.result = React.createElement(tag, null, [ fakeNode, stylesheetNode ]);
 });
+
+const functionalStylesheet = styleContent => func => {
+  const tag = getTag(func);
+  const stylesheetNode = getStylesheet(func, styleContent);
+
+  // wrap rendered vnode with a hoc
+  return props => {
+    const newProps = getProps(props);
+    return React.createElement(tag, null, [ React.createElement(func, newProps, newProps.children), stylesheetNode ]);
+  };
+};
+
+export const stylesheet = (styles, functional) => functional ? functionalStylesheet(styles)(functional): renderStylesheet(styles);
